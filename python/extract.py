e511f3f217e961ac2002384530da32901b8c8dca
Aligbe Emmanuel <aligbe.emmy@gmail.com>
2026-05-20 11:42:52 +0100

    add pyhton asignment file

--- python/extract.py
+++ python/extract.py
@@ -2,7 +2,6 @@ def extract_characters(name, n):
     # create a variable to store the extracted characters
     result_name = ""
 
-
     # index from the arguemet strings
     index = 0
 
---
+++ python/python_assignment.py
@@ -0,0 +1,6 @@
+def reverse_name(name):
+    return name[::-1]
+
+if __name__ == "__main__":
+    name = input('Enter string: ')
+    print(reverse_name('Emmanuel'))
