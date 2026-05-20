db1390810b9cdeb86fbae80a7470d66b7bf1216b
Aligbe Emmanuel <aligbe.emmy@gmail.com>
2026-05-20 16:44:06 +0100

    add code for 2e

--- python/assignment2-3.py
+++ python/assignment2-3.py
@@ -14,3 +14,12 @@ print("subList1 after removal: ", subList1)
 subList1.extend(subList2)
 mergedList = subList1
 print("Merged list: ", mergedList)
+
+salaryList = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
+print("Salary list: ", salaryList)
+
+for s in salaryList:
+    salaryList = s * 1.04
+    print(salaryList)
+
+    
